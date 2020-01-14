ec37f409a8a153caf6b401a11eeb87142b0f8ed0
jared-w <jaredweakly@gmail.com>
2020-01-13 22:34:17 -0800

    Simplify nix files; give up on haskell.nix for now

--- default.nix
+++ default.nix
@@ -1,32 +1,9 @@
-let
-  pkgs = import (fetchTarball
-    "https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz")
-    { };
-  gitignoreSrc = pkgs.fetchFromGitHub {
-    owner = "hercules-ci";
-    repo = "gitignore";
-    rev = "f9e996052b5af4032fe6150bba4a6fe4f7b9d698";
-    sha256 = "sha256:0jrh5ghisaqdd0vldbywags20m2cxpkbbk5jjjmwaw0gr8nhsafv";
-  };
-  inherit (import gitignoreSrc { inherit (pkgs) lib; }) gitignoreSource;
-in let
-  config = {
-    packageOverrides = pkgs: rec {
-      haskellPackages = pkgs.haskellPackages.override {
-        overrides = self: super: rec {
-          site = pkgs.haskell.lib.addExtraLibraries
-            (self.callCabal2nix "jaredweakly" (gitignoreSource ./.) { })
-            (with pkgs.nodePackages; [ html-minifier serve ]);
-        };
-      };
-    };
-  };
-  pkgs = import (fetchTarball
-    "https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz") {
-      inherit config;
-    };
-in if pkgs.lib.inNixShell then
-  pkgs.haskellPackages.site.env
-else {
-  site = pkgs.haskellPackages.site;
+{ pkgs ? import <nixpkgs> { } }:
+let nodePkgs = (with pkgs.nodePackages; [ html-minifier serve ]);
+in pkgs.haskellPackages.developPackage {
+  name = "jaredweakly";
+  root = ./.;
+  modifier = drv:
+    pkgs.haskell.lib.addBuildTools drv
+    (with pkgs.haskellPackages; [ cabal-install ghcid ] ++ nodePkgs);
 }
